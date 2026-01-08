10c7050ffa23ba3dcd4861262208fc9b52d16c6f
kranthisudhak <kranthisudhak@gmail.com>
2026-01-08 20:21:46 +0530

    Update prompt text for number guessing game

--- number.py
+++ number.py
@@ -3,7 +3,7 @@ import random
 secret_number = random.randint(1, 100)
 
 print("Welcome to the Number Guessing Game!")
-print("I'm thinking of a number between 1 and 100.")
+print("Think a number between 1 and 100.")
 
 
 while True:
